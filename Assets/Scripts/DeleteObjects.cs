fa8f1e1b57508d154c1c6af6acdda4411b5f0851
enrisco <enriquerodrigues790@gmail.com>
2022-04-19 22:20:01 -0300

    tristeza

--- Assets/Scripts/DeleteObjects.cs
+++ Assets/Scripts/DeleteObjects.cs
@@ -15,12 +15,13 @@ class DeleteObjects : MonoBehaviour
 
     private void Update()
     {
+        if(transform.position == Vector3.zero) GameObject.Find("SpawnerParent").GetComponent<SpawnerController>().Spawn();
         if (transform.position == MoveController.finalPosition) SpawnAndDestroy();
     }
 
     private void SpawnAndDestroy()
     {
-        GameObject.Find("").GetComponent<SpawnerController>().Spawn();
+        GameObject.Find("SpawnerParent").GetComponent<SpawnerController>().Spawn();
         Destroy(gameObject);
     }
 }
